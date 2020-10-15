3fc6f485d02b4b93e088a5999c2769c1a3884174
conesy4 <mconan@chicagobooth.edu>
2020-10-15 21:46:05 +0000

    Add and Subtract

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
 
+  def add_form
+    
+    render({ :template => "templates/add_form.html.erb"})
+  
+  end
+
+  def compute_addition
+
+    @num1 = params.fetch("first_num").to_f
+    @num2 = params.fetch("second_num").to_f
+    @result = @num1 + @num2
+    
+    render({ :template => "templates/add_results.html.erb"})
+  end
+
+  def subtract_form
+
+    render({ :template => "templates/subtract_form.html.erb"})
+  end
+
+  def compute_subtraction
+    @num1 = params.fetch("first_num").to_f
+    @num2 = params.fetch("second_num").to_f
+    @result = @num2 - @num1
+
+    render({ :template => "templates/subtract_results.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
 
+get("/wizard_add", { :controller => "application", :action => "compute_addition" })
+get("/wizard_subtract", { :controller => "application", :action => "compute_subtraction"})
+
+get("/add", { :controller => "application", :action => "add_form" })
+get("/subtract", { :controller => "application", :action => "subtract_form"})
+
+
+#in results
+#@first = params.fetch("first_num")
+#@second = params.fetch("second_num")
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
